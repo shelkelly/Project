a2e570964a2a33565d8fba46890c348b6cac8fc6
Philip Robbins <plrobbins68@outlook.com>
2020-10-24 13:33:06 -0400

    adding code for the api routes and also the books.js

---
+++ models/book.js
@@ -0,0 +1,15 @@
+// References the standard library
+var Sequelize = require("sequelize");
+//Connection to the the DB
+var sequelize = require("../config/connection.js");
+
+//Book matches the DB
+var Book = sequelize.define("book", {
+    title: Sequelize.STRING,
+    author: Sequelize.STRING,
+  });
+
+
+  Book.sync();
+// Book model is available for other files
+  module.exports = Book;
