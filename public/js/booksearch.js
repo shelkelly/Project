7724f51d95f9b32974b07da151c5793707fb02b2
Shelby <shelbypkelly@gmail.com>
2020-10-28 21:47:39 -0400

    search ajax function

--- public/js/booksearch.js
+++ public/js/booksearch.js
@@ -8,12 +8,11 @@ $(document).ready(function () {
 
     $(document).on("submit", searchbarInput, handleSearch);
 
-    function handleSearch(event) {
-        event.preventDefault();
-        
-        //function for getting search terms
-        $.get("/api/search/:term", function (data) {
-            results = data;
+    function handleSearch() {
+        //function for getting search terms from the database
+        $.get("/api/search/" + searchbarInput.val().trim(), function(data) {
+            console.log(data);
+            $("#card").append("<img src='" + small_img_url + "'>")
         })
     }
 
