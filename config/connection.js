f6516bd7ad7a5a4b996f62a20ed5059b6b5c9c8d
Philip Robbins <plrobbins68@outlook.com>
2020-10-29 22:13:06 -0400

    updating the code

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,20 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.PASSWORD,
-  database: "books"
+let connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "books"
 });
-
+}
 // Make connection.
-connection.connect(function(err) {
+connection.connect(err => {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
