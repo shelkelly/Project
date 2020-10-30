8584ae2a6ce1cd79d98b25f82bc100aa3e91a275
Philip Robbins <plrobbins68@outlook.com>
2020-10-29 22:00:20 -0400

    updating the code

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require('dotenv').config();
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "E122w920!",
-  database: "top_booksdb"
+  password: process.env.PASSWORD,
+  database: "books"
 });
 
 // Make connection.
