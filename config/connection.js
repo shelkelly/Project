bb2fce1b1395e978375a7b212fa2a6eb1c19fe1c
Philip Robbins <plrobbins68@outlook.com>
2020-10-30 20:30:46 -0400

    updating the css

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,6 @@
 const mysql = require("mysql");
 require('dotenv').config();
 
-<<<<<<< HEAD
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "E122w920!",
-  database: "books",
-  dialect: "mysql"
-});
-=======
->>>>>>> 84ff93879d9ce44cd1bc00fca2bcd1ce45bd3f07
-
 let connection
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
