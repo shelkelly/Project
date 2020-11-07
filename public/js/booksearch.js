d88bfa8bd8d566aa56321b3a3240c05eeffd1c37
Shelby <shelbypkelly@gmail.com>
2020-11-07 00:31:08 -0500

    styling updates

--- public/js/booksearch.js
+++ public/js/booksearch.js
@@ -12,12 +12,12 @@ $(document).ready(function () {
         $.get("/api/search/" + searchbarInput.val().trim(), function(data) {
             console.log(data);
             $('#book-area').empty();
-            const bookHolder = $("<div class='row'>")
+            const bookHolder = $("<div class='row'>");
             data.map(book=>{
-              return $(bookHolder).append(`<div class="col-sm-4"><img src="${book.small_image_url}"><h3>${book.title}</h3>${book.authors}</div>`)
+              return $(bookHolder).append(`<div class="col-sm-4"><img src="${book.small_image_url}"><h3>${book.title}</h3>${book.authors}</div>`);
             })
             $('#book-area').append(bookHolder)
-        })
-    }
+        });
+    };
 });
 
