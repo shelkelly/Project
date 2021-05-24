e75ffe473a30fbe7a1bfad28edc952db3212cec8
Shelby <shelbypkelly@gmail.com>
2021-05-24 19:12:20 -0400

    slight polishes to front end.

--- public/js/booksearch.js
+++ public/js/booksearch.js
@@ -6,18 +6,33 @@ $(document).ready(function () {
     var searchbarInput = $("#search");
     $(document).on("submit", searchbarInput, handleSearch);
     function handleSearch(event) {
-         event.preventDefault()
+        event.preventDefault()
         console.log(searchbarInput.val())
         //function for getting search terms from the database
-        $.get("/api/search/" + searchbarInput.val().trim(), function(data) {
+        $.get("/api/search/" + searchbarInput.val().trim(), function (data) {
             console.log(data);
             $('#book-area').empty();
             const bookHolder = $("<div class='row'>");
-            data.map(book=>{
-              return $(bookHolder).append(`<div class="col-sm-4"><img src="${book.small_image_url}"><h3>${book.title}</h3>${book.authors}</div>`);
+            data.map(book => {
+                return $(bookHolder).append(`<div class="col-sm-4" id="searchItem"><img src="${book.small_image_url}"><h3>${book.title}</h3>${book.authors}</div>`);
             })
-            $('#book-area').append(bookHolder)
+            $('#book-area').append(bookHolder);
+
+
+
         });
+
+        $('#book-area').each(function(fadeInDiv) {
+            $(this).delay(fadeInDiv * 500).fadeIn(1000);
+          });
+
+
+
+        function fadeInPlaylist(elem) {
+            elem.css('opacity',1);
+        }
+
+
     };
 });
 
